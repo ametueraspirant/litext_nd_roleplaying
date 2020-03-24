c40f7c0c4299038a10c67c4f31274d0de0d8d859
Amet <ametueraspirant@yahoo.com>
2020-03-23 20:59:23 -0600

    uninstall lodash

--- src/pages/Forum.js
+++ src/pages/Forum.js
@@ -4,19 +4,32 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function Forum(props)
 {
-	const [post, getPost] = useState
-	const getForum = event => {
-		event.preventDefault();
-		axios.get('/api/forum')
-		.then(res=> {
-			if(res.status === 200){
-				console.log("got the forum", res);
-			};
-		});
-	};
+	const [forum, setForum] = useState({
+		title: "",
+		description: "",
+		subforums: [],
+		threads: []
+	});
+
+	useEffect(()=> {
+
+	}, []);
+
+	// const getForum = event => {
+	// 	event.preventDefault();
+	// 	axios.get('/api/forum')
+	// 	.then(res=> {
+	// 		if(res.status === 200){
+	// 			console.log("got the forum", res);
+	// 		};
+	// 	});
+	// };
 
 	return(
-		<button onClick = {getForum}>test</button>
+		<h1>{forum.title?forum.title:"no forum saved"}</h1>
+		<h1>{forum.description?forum.description:"no forum saved"}</h1>
+		<h1>{forum.subforums?forum.subforums:"no forum saved"}</h1>
+		<h1>{forum.threads?forum.threads:"no forum saved"}</h1>
 	);
 }
 
